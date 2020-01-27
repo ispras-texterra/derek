8ac32c892e371cf97dd5980697fce1be857281ed
trifonov <trifonov@ispras.ru>
2020-01-27 17:57:26 +0300

    added dropout before classifier in NER neural network

--- derek/ner/nn/graph_factory.py
+++ derek/ner/nn/graph_factory.py
@@ -19,6 +19,7 @@ def _build_graph(props: dict, out_size: int, shared_encoding, shared_inputs, opt
     gold_labels = tf.placeholder(tf.int32, shape=[None, None], name='labels')
     graph["inputs"]["labels"] = gold_labels
 
+    shared_encoding = tf.nn.dropout(shared_encoding, graph["inputs"]["dropout_rate"])
     loss, label, _ = get_seq_labelling_loss(shared_encoding, seq_len, gold_labels, out_size, props)
     train_op = get_train_op(loss, optimizer, props)
 
